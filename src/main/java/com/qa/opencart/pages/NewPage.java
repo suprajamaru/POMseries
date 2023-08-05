63754336c802b35abc703ad9d93a4b9a381d4423
suprajamaru <140737932+suprajamaru@users.noreply.github.com>
2023-08-06 00:40:15 +0530

    Create NewPage.java

---
+++ src/main/java/com/qa/opencart/pages/NewPage.java
@@ -0,0 +1,10 @@
+package com.qa.opencart.pages;
+
+public class NewPage {
+	public static void main(String[] args) {
+		
+	}
+public void login() {
+	System.out.println("maruthi login method");
+}
+}
